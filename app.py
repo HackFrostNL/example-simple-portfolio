bd0907aa79e23d2cddea5c7f464d5d03315cf8db
Jack Arthur Harrhy <me@jackharrhy.com>
2021-07-18 14:05:58 -0230

    filled out the basic portfolio structure, with github fetching, and weird pepper image

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
-from flask import Flask
-from markupsafe import escape
+from flask import Flask, render_template
+import requests
+
+name = "Jack"
+
+github_projects_url = "https://api.github.com/users/jackharrhy/repos"
+projects_from_github = requests.get(github_projects_url).json()
+
+projects = []
+
+for project in projects_from_github:
+    name = project["name"]
+    desc = project["description"]
+    url = project["html_url"]
+
+    projects.append({
+        "name": name,
+        "desc": desc,
+        "url": url,
+    })
+
+list_of_food = [
+    "Tea",
+    "Apples",
+    "Ramen"
+]
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def about_page():
+    return render_template("about.html", name=name, list_of_food=list_of_food)
 
-@app.route("/<name>")
-def hello(name):
-    return f"Hello, {escape(name)}!"
+@app.route("/projects")
+def projects_page():
+    return render_template("projects.html", name=name, projects=projects)
+
+@app.route("/contact")
+def contact_page():
+    return render_template("contact.html", name=name)
